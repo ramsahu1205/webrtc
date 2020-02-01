9b73ba9f55b9c4dd869c2e179cb608221aadd0dd
Ram Sahu <sahu1205.ramchandra@gmail.com>
2020-02-02 02:03:06 +0530

    2nd commit

--- indexAndroid.js
+++ indexAndroid.js
@@ -16,39 +16,18 @@ wsServer = new WebSocketServer({
 });
 
 // WebSocket server
+var con={}
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
   var index = clients.push(connection) - 1;
   console.log('Index: ' + index)
-
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
   connection.on('message', function(message) {
-    var json = JSON.stringify(message, null, 4);
-    var type = JSON.parse(message['utf8Data'])['type'];
-    console.log(type);
-    console.log(clients.length)
-    if ((type == 'OFFER') && (clients.length == 2)) {
-        console.log(clients.length)
-        clients[1].send(JSON.stringify(message))
-        console.log('OFFER!')
-    } else if (type == 'ANSWER') {
-        clients[0].send(JSON.stringify(message))
-        console.log('ANSWER!')
-    } else if (type != 'OFFER'){
-        for (var i=0; i<clients.length; i++) {
-            // console.log(clients[i])
-            clients[i].send(JSON.stringify(message));
-        }
+    var data=JSON.parse(message)
+    if(data.type=="candidate"){
+      console.log(message);
+      con[data["userId"]]=data;
     }
-    if ((type == 'OFFER') && (clients.length < 2)) {
-        console.log("offer saved")
-        offer = message;
-    }
-    if (clients.length >= 2) {
-        clients[1].send(JSON.stringify(offer))
-        console.log('OFFER!')
-    }
+  
   });
 
   connection.on('close', function(connection) {
